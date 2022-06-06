72105897b05e19fa3ee040c89dfa7718c7a063e0
SulaimanS <mirsulaimansultan@gmail.com>
2022-06-06 18:33:14 -0400

    need to merge

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sys
 
 pygame.init()
 # WINDOW & DISPLAY SETTINGS
-windowSurface = pygame.display.set_mode((1200,800),0,32)
-windowSurface.fill((255,255,255))
+windowSurface = pygame.display.set_mode((1200, 800), 0, 32)
+windowSurface.fill((255, 255, 255))
 windowRect = windowSurface.get_rect()
 windowCenter = windowRect.center
 pygame.display.set_caption("Travel to Monaco")
@@ -15,20 +15,25 @@ pygame.display.set_caption("Travel to Monaco")
 # INDEX
 index = 0
 # CHARACTERS
-wilfrid_walk2 = pygame.image.load(r"P\pygame\hackathon1\Pixel_Art\character\sandbox_walk.png")
-wilfrid_walk1 = pygame.image.load(r"P\pygame\hackathon1\Pixel_Art\character\sandbox_result_i.png")
+wilfrid_walk2 = pygame.image.load(
+    r"P\pygame\hackathon1\Pixel_Art\character\sandbox_walk.png"
+)
+wilfrid_walk1 = pygame.image.load(
+    r"P\pygame\hackathon1\Pixel_Art\character\sandbox_result_i.png"
+)
 wilfrid_walks = [wilfrid_walk1, wilfrid_walk2]
-wilfridRect = wilfrid_walk1.get_rect(bottomleft = (0,700))
+wilfridRect = wilfrid_walk1.get_rect(bottomleft=(0, 700))
 bob = 0
 
 
 # LANDSCAPE
-background1 = pygame.image.load(r"P\pygame\hackathon1\Pixel_Art\scene1\zxcr_s9nj_200605 - Copy.jpg")
-background1rect = background1.get_rect(topleft = (0,0))
+background1 = pygame.image.load(
+    r"P\pygame\hackathon1\Pixel_Art\scene1\zxcr_s9nj_200605 - Copy.jpg"
+)
+background1rect = background1.get_rect(topleft=(0, 0))
 
 # EXTRA
 clock = pygame.time.Clock()
-
 
 
 # GAME LOOP
@@ -44,20 +49,21 @@ while True:
     windowSurface.blit(background1, background1rect)
     background1rect.right -= 2
     # NON STATIC BLITS (CHARACTERS +)
-    windowSurface.blit(wilfrid_walks[bob],wilfridRect)
+    windowSurface.blit(wilfrid_walks[bob], wilfridRect)
 
     # COLLISIONS & LOGIC
     if background1rect.right < 1200:
-        background1rect.topleft = (0,0)
+        background1rect.topleft = (0, 0)
 
     index += 1
-    if index > 1000: index = 0
+    if index > 1000:
+        index = 0
     if index % 5 == 0:
         if bob == 0:
             bob = 1
         elif bob == 1:
             bob = 0
 
-    #NECESSITIES
+    # NECESSITIES
     clock.tick(60)
     pygame.display.update()
