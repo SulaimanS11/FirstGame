ec9dde0b2dbf8db7feff9e9f6d64a9b074494d10
SulaimanS <mirsulaimansultan@gmail.com>
2022-06-30 17:21:17 -0400

    more wayly things, adding and more movement commands

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame.locals import *
 import sys
-import time
 
 
 pygame.init()
@@ -22,18 +21,22 @@ def wilfird_movement():
             bob = 0
 
 
-def wayly_animation():
+def wayly_movement():
     global wayly1Rect, rob
     if index % 15 == 0:
         if rob == 0:
             rob = 3
         rob -= 1
-    
-def scoring():
-    global score, index
-    if index % 60 == 0:
-        score += 1
-        print(score)
+
+
+# def player_walk_animation():
+#     global wilfridRect, SCREEN_LENGTH
+
+#     if wilfridRect.x >= SCREEN_LENGTH:
+#         wilfridRect.x = SCREEN_LENGTH
+#     if wilfridRect.x <= 0:
+#         wilfridRect.x = 0
+
 
 # WINDOW & DISPLAY SETTINGS
 SCREEN_HEIGHT = 384
@@ -50,54 +53,40 @@ index = 0
 
 
 # CHARACTERS
-
-### WILFRID
 wilfrid_walk2 = pygame.image.load(
-    r"P\pygame\hackathon1\Pixel_Art\character\sandbox_walk.png"
+    r"P\hackathons\1sthackathon\Pixel_Art\character\sandbox_walk.png"
 )
 wilfrid_walk1 = pygame.image.load(
-    r"P\pygame\hackathon1\Pixel_Art\character\sandbox_result_i.png"
+    r"P\hackathons\1sthackathon\Pixel_Art\character\sandbox_result_i.png"
 )
 wilfrid_walks = [wilfrid_walk1, wilfrid_walk2]
-wilfridRect = wilfrid_walk1.get_rect(bottomleft=(0, 375))
+wilfridRect = wilfrid_walk1.get_rect(bottomleft=(0, 384))
 bob = 0
-horizontal_speed = 0
-vertical_speed = 0
-bobby = wilfridRect.y
-red = 0
+player_speed = 0
 
-### WAYLY
+
 wayly_still = pygame.image.load(
-    r"P\pygame\hackathon1\Pixel_Art\enemies\enemyuno\gorlrun.png"
+    r"P\hackathons\1sthackathon\Pixel_Art\enemies\enemyuno\gorlrun.png"
 ).convert_alpha()
 wayly_run1 = pygame.image.load(
-    r"P\pygame\hackathon1\Pixel_Art\enemies\enemyuno\gorlrun2.png"
+    r"P\hackathons\1sthackathon\Pixel_Art\enemies\enemyuno\gorlrun2.png"
 ).convert_alpha()
 wayly_run2 = pygame.image.load(
-    r"P\pygame\hackathon1\Pixel_Art\enemies\enemyuno\gorlrun3.png"
+    r"P\hackathons\1sthackathon\Pixel_Art\enemies\enemyuno\gorlrun3.png"
 ).convert_alpha()
 wayly = [wayly_still, wayly_run1, wayly_run2, wayly_still]
-wayly1Rect = wayly_still.get_rect(bottomleft=(1300, 375))
+wayly1Rect = wayly_still.get_rect(bottomleft=(1300, 384))
 rob = 0
-wayly_horspeed = -4
-wayly_vertspeed = 0
 
 # LANDSCAPE
 background1 = pygame.image.load(
-    r"P\pygame\hackathon1\Pixel_Art\scene4\bobsandvegen.jpg"
+    r"P\hackathons\1sthackathon\Pixel_Art\scene4\bobsandvegen.jpg"
 )
 background1rect = background1.get_rect(topleft=(0, 0))
 
 # EXTRA
 clock = pygame.time.Clock()
 
-# SCORE
-score = 0
-
-def waylyAttack():
-    global wayly_speed, wayly_horspeed, wayly1Rect, score
-    if score == -4:
-        None
 
 # GAME LOOP
 while True:
@@ -109,25 +98,15 @@ while True:
         # PLAYER CONTROLS
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
-                horizontal_speed += 5
-                red = 1
+                player_speed += 5
             if event.key == pygame.K_a:
-                horizontal_speed -= 5
-                red = 1
-            if event.key == pygame.K_w:
-                vertical_speed -= 10
-                red = 1
+                player_speed -= 5
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
-                horizontal_speed -= 5
+                player_speed -= 5
             if event.key == pygame.K_a:
-                horizontal_speed += 5
-            if event.key == pygame.K_w:
-                vertical_speed += 10
-
-    # SCORE
-    scoring()
+                player_speed += 5
 
     # STATIC BLITS
 
@@ -135,54 +114,24 @@ while True:
     windowSurface.blit(background1, background1rect)
 
     # NON STATIC BLITS (CHARACTERS +)
-    if red == 0:
-        windowSurface.blit(wilfrid_walks[0], wilfridRect)
-    else:
-        windowSurface.blit(wilfrid_walks[bob], wilfridRect)
+    windowSurface.blit(wilfrid_walks[bob], wilfridRect)
     windowSurface.blit(wayly[rob], wayly1Rect)
 
     ### WAYLY MOVEMENT
-
-    wayly1Rect.x += wayly_horspeed
-    wayly1Rect.y += wayly_vertspeed
-    if wayly1Rect.left < -20:
-        wayly1Rect.bottomleft = (1300, 375)
+    wayly1Rect.x -= 4
 
     ### WILFRID MOVEMENT
-    wilfridRect.x += horizontal_speed
-    wilfridRect.y += vertical_speed
-    if horizontal_speed == 0 and vertical_speed == 0:
-        red = 0
-    else:
-        red = 1
+    wilfridRect.x += player_speed
 
     # COLLISIONS & LOGIC
     if background1rect.right < 900:
         background1rect.topleft = (0, 0)
-    
-    ### CEILING
-    if wilfridRect.top <= 0:
-        wilfridRect.top = 0
-
-    ### SCREEN WRAPPING
-    if wilfridRect.right <= 0:
-        wilfridRect.left = SCREEN_LENGTH
-
-    elif wilfridRect.left >= SCREEN_LENGTH:
-        wilfridRect.right = 0
-    
-    ### GRAVITY
-    if vertical_speed == 0:
-        if wilfridRect.bottomleft[1] < 375:
-            wilfridRect.y += 10
-        else:
-            wilfridRect.y = bobby
 
     ### main char movement code
     index_indexer()
     wilfird_movement()
-    wayly_animation()
-
+    wayly_movement()
+    # player_walk_animation()
 
     # NECESSITIES
     clock.tick(60)
